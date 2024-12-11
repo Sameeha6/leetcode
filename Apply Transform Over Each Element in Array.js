a06474103065be3a10490be23fdcef4e00efe729
Sameeha T <sameehasirajt@gmail.com>
2024-12-11 23:37:00 +0530

    Apply Transform Over Each Element in Array

---
+++ Apply Transform Over Each Element in Array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    newArray=[]
+    for(let i=0;i<arr.length;i++)
+        newArray[i]=fn(arr[i],i)
+    return newArray
+};
